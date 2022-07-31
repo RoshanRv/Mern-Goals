8cfe7ca5849cdb8e69bc4d66fe9f2b58c51bf8cf
Roshan <nkroshankumar@gmail.com>
2022-07-31 16:51:21 +0530

    added spinner

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='w-16 h-16 mx-auto rounded-full border-4 border-black border-t-gray-500 animate-spin' ></div>
+  )
+}
+
+export default Spinner
